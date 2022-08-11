25164c38e430cf5d95c1bc8f435e1c562863f97e
zhangyu <9194964+zhangyu0816@user.noreply.gitee.com>
2022-08-11 12:40:32 +0800

    no message

--- app/src/main/java/com/yimi/rentme/vm/fragment/MainMineViewModel.kt
+++ app/src/main/java/com/yimi/rentme/vm/fragment/MainMineViewModel.kt
@@ -1,23 +1,29 @@
 package com.yimi.rentme.vm.fragment
 
+import android.Manifest
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
 import android.os.Handler
 import android.os.SystemClock
 import android.view.View
 import android.view.animation.Animation
+import android.widget.Toast
 import com.yimi.rentme.MineApp
 import com.yimi.rentme.R
 import com.yimi.rentme.activity.FCLActivity
 import com.yimi.rentme.activity.OpenVipActivity
+import com.yimi.rentme.activity.SelectImageActivity
 import com.yimi.rentme.databinding.FragMainMineBinding
+import com.yimi.rentme.dialog.SelectorDF
 import com.yimi.rentme.dialog.VipAdDF
 import com.yimi.rentme.fragment.MemberDiscoverFrag
 import com.yimi.rentme.fragment.MemberVideoFrag
 import com.yimi.rentme.vm.BaseViewModel
 import com.zb.baselibs.app.BaseApp
+import com.zb.baselibs.dialog.RemindDF
 import com.zb.baselibs.utils.getInteger
 import com.zb.baselibs.utils.getLong
+import com.zb.baselibs.utils.permission.requestPermissionsForResult
 import com.zb.baselibs.utils.saveInteger
 import com.zb.baselibs.views.replaceFragment
 import org.jetbrains.anko.runOnUiThread
@@ -43,8 +49,12 @@ class MainMineViewModel : BaseViewModel() {
             mHandler.postDelayed(this, 500L)
         }
     }
+    private var dataList = ArrayList<String>()
 
     override fun initViewModel() {
+        dataList.add("发布照片")
+        dataList.add("发布小视频")
+
         binding.mineInfo = MineApp.mineInfo
         binding.contactNum = MineApp.contactNum
         binding.hasNewBeLike =
@@ -225,7 +235,71 @@ class MainMineViewModel : BaseViewModel() {
     /**
      * 上传动态
      */
-    fun publishDiscover(view: View) {}
+    fun publishDiscover(view: View) {
+        if (checkPermissionGranted(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            )
+        ) {
+            toSelectImage()
+        } else {
+            if (getInteger("image_permission", 0) == 0) {
+                saveInteger("image_permission", 1)
+                RemindDF(activity).setTitle("权限说明")
+                    .setContent(
+                        "在使用发布动态功能，包括图文、视频时，我们将会申请相机、存储、麦克风权限：" +
+                                "\n 1、申请相机权限--发布动态时获取拍摄照片，录制视频功能，" +
+                                "\n 2、申请存储权限--发布动态时获取保存和读取图片、视频，" +
+                                "\n 3、申请麦克风权限--发布视频动态时获取录制视频音频功能，" +
+                                "\n 4、若您点击“同意”按钮，我们方可正式申请上述权限，以便正常发布图文动态、视频动态，" +
+                                "\n 5、若您点击“拒绝”按钮，我们将不再主动弹出该提示，您也无法使用发布动态功能，不影响使用其他的虾菇功能/服务，" +
+                                "\n 6、您也可以通过“手机设置--应用--虾菇--权限”或app内“我的--设置--权限管理--权限”，手动开启或关闭相机、存储、麦克风权限。"
+                    ).setSureName("同意").setCancelName("拒绝")
+                    .setCallBack(object : RemindDF.CallBack {
+                        override fun sure() {
+                            toSelectImage()
+                        }
+                    }).show(activity.supportFragmentManager)
+            } else {
+                Toast.makeText(
+                    activity,
+                    "可通过“手机设置--应用--虾菇--权限”或app内“我的--设置--权限管理--权限”，手动开启或关闭相机权限、存储权限、麦克风权限。",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    /**
+     * 选择发布动态
+     */
+    private fun toSelectImage() {
+        launchMain {
+            activity.requestPermissionsForResult(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO, rationale = "为了更好的提供服务，需要获取相机权限、存储权限、麦克风权限"
+            )
+            SelectorDF(activity).setDataList(dataList).setCallBack(object : SelectorDF.CallBack {
+                override fun sure(position: Int) {
+                    if (position == 0) {
+                        activity.startActivity<SelectImageActivity>(
+                            Pair("isMore", true),
+                            Pair("isPublish", true)
+                        )
+                    } else {
+                        activity.startActivity<SelectImageActivity>(
+                            Pair("showVideo", true),
+                            Pair("isPublish", true)
+                        )
+                    }
+                }
+            }).show(activity.supportFragmentManager)
+        }
+    }
 
     /**
      * 显示爱情盲盒
